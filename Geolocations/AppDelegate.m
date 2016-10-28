7619e697b795cad6130d590176bf7c23c38a4960
Jin Sun <jsun@maxleap.com>
2016-10-28 11:34:23 +0800

    remove error

--- Geolocations/AppDelegate.m
+++ Geolocations/AppDelegate.m
@@ -17,9 +17,8 @@
     // ****************************************************************************
     // Uncommit fill in with your MaxLeap credentials:
     // ****************************************************************************
-#error Please fill in with your MaxLeap credentials
+#warning Please Uncommit and fill in with your MaxLeap credentials
     // [MaxLeap setApplicationId:@"APPLICATION_ID_HERE" clientKey:@"CLIENT_KEY_HERE"];
-    [MaxLeap setApplicationId:@"55b202f760b2173daeff03b1" clientKey:@"Yy1VSEJlUHNnQzFRcHBudTAzWGhSdw" site:MLSiteCN];
     
     // Override point for customization after application launch.
     return YES;
